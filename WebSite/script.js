21233446e346c4ab89891ad75758029f66d6413e
MarkoSelchanec <59167796+MarkoSelchanec@users.noreply.github.com>
2020-05-23 10:47:38 +0200

    applying makeup

--- WebSite/script.js
+++ WebSite/script.js
@@ -1,5 +1,3 @@
-// our js script
-let btn = document.getElementsByTagName("button")[0]
-let par = document.getElementsByTagName("p")[0]
-console.log(par)
-btn.onclick = () => par.style.color = "red"
+/* ==========
+VIDEO SECTION
+*/
