9b3a6edfec54a111afe4cac636e89b6079c737cb
MarkoSelchanec <59167796+MarkoSelchanec@users.noreply.github.com>
2020-05-23 12:06:12 +0200

    working on it

--- WebSite/script.js
+++ WebSite/script.js
@@ -5,10 +5,9 @@ VIDEO SECTION
 const prevBtn = document.getElementById("prev_btn");
 const nextBtn = document.getElementById("next_btn");
 
-const prevVid = document.getElementById("prev_video");
-console.log(prevVid)
-
-const videoNum = 0;
+let prevVid = document.getElementById("prev_video");
+let midVid = document.getElementById("mid_video");
+let nextVid = document.getElementById("next_video");
 
 const videoList = [
     "https://www.youtube.com/embed/wmOW091P2ew",
@@ -17,9 +16,24 @@ const videoList = [
     "https://www.youtube.com/embed/UznxKiHrNx4"
 ]
 
-let vidSection = {
-    prevVid : videoList[videoNum],
-    midVid : videoList[videoNum + 1],
-    nextVid : videoList[videoNum + 2]
+let videoNum = 0;
+
+prevVid.setAttribute("src", videoList[videoNum]);
+midVid.setAttribute("src", videoList[videoNum + 1]);
+nextVid.setAttribute("src", videoList[videoNum + 2]);
+
+let videoPos = function(prev, mid, next, vidNum) {
+    prev.setAttribute("src", videoList[vidNum])
+    mid.setAttribute("src", videoList[vidNum + 1])
+    next.setAttribute("src", videoList[vidNum + 2])
 }
 
+nextBtn.addEventListener("click", function() {
+    videoNum++
+    videoPos(prevVid, midVid, nextVid, videoNum);
+})
+
+prevBtn.addEventListener("click", function() {
+    videoNum--
+    videoPos(prevVid, midVid, nextVid, videoNum);
+})
