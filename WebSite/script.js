156f5278b842d89a5aba0a85af993af621b84c7e
MarkoSelchanec <59167796+MarkoSelchanec@users.noreply.github.com>
2020-05-23 11:31:24 +0200

    working

--- WebSite/script.js
+++ WebSite/script.js
@@ -1,3 +1,25 @@
 /* ==========
 VIDEO SECTION
 */
+
+const prevBtn = document.getElementById("prev_btn");
+const nextBtn = document.getElementById("next_btn");
+
+const prevVid = document.getElementById("prev_video");
+console.log(prevVid)
+
+const videoNum = 0;
+
+const videoList = [
+    "https://www.youtube.com/embed/wmOW091P2ew",
+    "https://www.youtube.com/embed/X9ivR4y03DE",
+    "https://www.youtube.com/embed/wZAjVQWbMlE",
+    "https://www.youtube.com/embed/UznxKiHrNx4"
+]
+
+let vidSection = {
+    prevVid : videoList[videoNum],
+    midVid : videoList[videoNum + 1],
+    nextVid : videoList[videoNum + 2]
+}
+
